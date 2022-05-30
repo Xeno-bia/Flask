eeee28c3435a854869b1e08215ac8e5c51301040
Xeno-bia <98762638+Xeno-bia@users.noreply.github.com>
2022-05-31 01:57:46 +0900

    Add files via upload

--- project/app.py
+++ project/app.py
@@ -1,23 +1,27 @@
 """
 project
-├ app.py
-├ templates
-│　└ index.html
-└ static
-    └ style.css
+    app.py
+    templates
+        index.html
+    static
+        style.css
 """
 
 from flask import *
 
 app = Flask(__name__)
 
-@app.route("URL末尾")
-def メソッド():
-    return render_template("HTMLファイル", 変数 = 値)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
 
 """
-$env:FLASK_APP = "app.py"
-$env:FLASK_DEBUG = 0 or 1
-$ flask run
-CTRL + C
+> $env:FLASK_APP = "app.py"
+> $env:FLASK_DEBUG = 1
+> flask run
+> CTRL + C
 """
